eb15b369039481e8c0c8b135bf32e165a374d0c3
Jdelorim <joshuadelorimier@gmail.com>
2018-09-14 14:46:41 -0400

    parallax scrolling

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-    $(".sidenav").sidenav();
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var sideNav = document.querySelector(".sidenav");
+    M.Sidenav.init(sideNav, {});
+
+    var jparallax = document.querySelectorAll('.parallax');
+     M.Parallax.init(jparallax, {
+        responsiveThreshold: 0
+     });
+     
+});
+*/
+
+$(document).ready(function(){
+    console.log('hello');
+    
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+
 });
